17762cbce4933a442f8e8149d4f84da6c1254d2d
Edison Lee <edisonlee@edisonlee55.com>
2018-02-13 17:20:52 +0800

    Add Welcome

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ client.on('message', message => {
 });
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('welcome');
+  const channel = member.guild.channels.find('general');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
