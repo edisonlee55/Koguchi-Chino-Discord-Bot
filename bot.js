978f93e944c352a58ff14da48e74c09dae87314e
Edison Lee <edisonlee@edisonlee55.com>
2018-02-15 09:51:18 +0800

    Add eval

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', message => {
               title: "Return:",
               description: "```" + clean(evaled) + "```",
               footer: {
-                text: "Asked by: " + message.author.username + " (" + message.author.id + ")"
+                text: "Asked by: <@" + message.author.id + "> (" + message.author.id + ")"
               }
             }
           });
